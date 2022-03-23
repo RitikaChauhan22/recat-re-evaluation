d79832d17ac4ca296b79a48772ccea85fc40eab6
RitikaChauhan22 <91660344+RitikaChauhan22@users.noreply.github.com>
2022-03-23 19:44:18 +0530

    n

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -4,15 +4,24 @@ import { About } from "../components/About";
 import { Home } from "../components/Home";
 import { Products } from "../components/ProductsDashboard";
 import { SingleProductList } from "../components/SingleProduct";
+
+
 export const MainRoutes = () => {
+
   return (
+
     <Routes>
+
       <Route path="/" element={<App />}>
         <Route path="" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<SingleProductList />} />
+        <Route path="products/" element={<SingleProductList />} />
+
       </Route>
+
     </Routes>
+    
   );
+
 };
