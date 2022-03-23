f7e794bd382bc0ea6812f53ecb0bafd87e844fde
RitikaChauhan22 <91660344+RitikaChauhan22@users.noreply.github.com>
2022-03-23 18:25:11 +0530

    b

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar.module";
 import { Outlet } from "react-router-dom";
 
 function App() {
